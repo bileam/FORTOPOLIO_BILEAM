3ded9ebc940cba8256ee54d16a60f54ac2111627
bileam mangalla <bileammangalla12@gmail.com>
2026-02-28 22:25:10 +0700

    AOS

--- src/Pages/Opening.jsx
+++ src/Pages/Opening.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const Opening = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2500,
+      easing: "ease-in-out",
+      once: true, // animasi hanya sekali
+    });
+  }, []);
   return (
     <div
       className="h-screen bg-linear-to-b from-black via-[#312e81] to-[#a855f7]
           text-white flex items-center justify-center"
     >
       <h1
-        className="
+        data-aos="fade-up"
+        className=" 
     text-[5rem] md:text-[6rem] font-extrabold
     text-[#e9d5ff]
     drop-shadow-[0_0_30px_rgba(168,85,247,0.8)]
