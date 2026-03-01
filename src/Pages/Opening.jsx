ac8f73c0ae6a35b726449ad3034066d07b4301a3
bileam mangalla <bileammangalla12@gmail.com>
2026-03-01 17:08:15 +0700

    update opengin

--- src/Pages/Opening.jsx
+++ src/Pages/Opening.jsx
@@ -12,13 +12,13 @@ const Opening = () => {
   return (
     <div
       className="h-screen bg-linear-to-b from-black via-[#312e81] to-[#a855f7]
-          text-white flex items-center justify-center md:px-0 px-4"
+          text-white flex items-center justify-center md:px-0 px-4 "
     >
       <h1
         data-aos="fade-up"
         className=" 
-    text-[4rem]   md:text-[6rem] font-extrabold  animate-float
-    text-[#e9d5ff]
+    text-[4rem]   md:text-[6rem] font-extrabold 
+    text-[#e9d5ff] 
     drop-shadow-[0_0_30px_rgba(168,85,247,0.8)]
     drop-shadow-[0_0_60px_rgba(99,102,241,0.6)]
   "
